ef7376c33ffa0b13c5fc06ba6825373b9ac8dde8
Panji Sastra Jingga <panjisastra89@gmail.com>
2025-12-24 18:57:59 +0700

    Change API URL in ApiService
    Updated the API URL in ApiService.

--- lib/app/data/services/api_service.dart
+++ lib/app/data/services/api_service.dart
@@ -4,7 +4,6 @@ import '../models/patient_model.dart';
 class ApiService {
   final Dio _dio = Dio();
 
-  // GANTI jika ingin API lain
   final String _url = 'https://dummyjson.com/c/b3ca-4cfb-47cb-b7a5';
 
   Future<List<PatientModel>> fetchPatients() async {
